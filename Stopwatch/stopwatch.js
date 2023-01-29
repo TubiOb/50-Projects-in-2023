999208e6a3538edbbd88d94dd6ee46d6a68c570d
TubiOb <tubiobaloluwa@gmail.com>
2023-01-29 06:31:55 +0100

    finally working on the timer of the Stopwatch

--- Stopwatch/stopwatch.js
+++ Stopwatch/stopwatch.js
@@ -7,4 +7,38 @@ window.addEventListener('load', () => {
     var Tens = 00;
     var Seconds = 00;
     var interval;
+
+
+    function timerCount() {
+        tens++;
+
+        if (tens > 9) {
+            tens.innerHTML = tens;
+        } else if (tens <= 9) {
+            tens.innerHTML = "0" + tens;
+        } else if (tens > 99) {
+            console.log(secs)
+            secs++;
+            console.log(secs)
+            tens = 0;
+            tens.innerHTML = "0" + 0;
+            secs.innerHTML = "0" + secs;
+        } else if (secs > 9) {
+            secs.innerHTML = secs;
+        }
+    }
+
+    let timeRun = false;
+    start.addEventListener('click', () => {
+        timeRun = !timeRun
+        if (timeRun) {
+            start.innerText = "Stop"
+            console.log("OYAAAAAA")
+            console.log(timeRun)
+        } else {
+            start.innerText = "Start"
+            console.log("I Don Start")
+            console.log(timeRun)
+        }
+    })
 })
