00cc73569b35ec51afb502e69ef9998996d78d1b
TubiOb <tubiobaloluwa@gmail.com>
2023-01-30 11:03:31 +0100

    Declaring the neccesary variables

--- Quiz Application/script.js
+++ Quiz Application/script.js
@@ -5,7 +5,14 @@ const ScorePage = document.querySelector("#scores");
 var highScoresTable = document.getElementById("high-scores");
 const UserInfo = document.querySelector("#userID");
 const User = document.getElementById("#username");
-const Enter = document.querySelector("#Submit");
+const Enter = document.querySelector("#enter");
+const Question = document.getElementById("question-text");
+const QuestionAnswer = document.querySelectorAll(".answer");
+const optionA = document.getElementById("answer1-text");
+const optionB = document.getElementById("answer2-text");
+const optionC = document.getElementById("answer3-text");
+const optionD = document.getElementById("answer4-text");
+
 
 window.addEventListener('load', () => {
 
