1eebd57b0e1c61288cc0c142de777f67eea8f130
TubiOb <tubiobaloluwa@gmail.com>
2023-01-30 10:18:33 +0100

    Javascript DOM elements

---
+++ Quiz Application/script.js
@@ -0,0 +1,12 @@
+const Home = document.querySelector("#home");
+const start = document.querySelector("#start-button");
+const highScore = document.querySelector("#score-button");
+const ScorePage = document.querySelector("#scores");
+var highScoresTable = document.getElementById("high-scores");
+const UserInfo = document.querySelector("#userID");
+const User = document.getElementById("#username");
+const Enter = document.querySelector("#Submit");
+
+window.addEventListener('load', () => {
+
+})
