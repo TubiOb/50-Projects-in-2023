a55cfd8998f01ba6904ba7992027d970af0f6ca4
TubiOb <tubiobaloluwa@gmail.com>
2023-02-02 02:02:51 +0100

    Corrected the id values of the buttons and made some adjustment in the javascript

--- Quiz Application/script.js
+++ Quiz Application/script.js
@@ -7,14 +7,7 @@ const userInfo = document.querySelector("#userID");
 const user = document.getElementById("username");
 const enter = document.querySelector("#enter");
 const quizPage = document.querySelector("#quiz");
-var timer = document.getElementById("timer");
-const question = document.getElementById("question-text");
-const questionAns = document.querySelectorAll(".answer");
-const optionA = document.getElementById("answer1-text");
-const optionB = document.getElementById("answer2-text");
-const optionC = document.getElementById("answer3-text");
-const optionD = document.getElementById("answer4-text");
-const submit = document.getElementById("submit");
+
 const optionsContainer = document.getElementById("opt");
 
 
@@ -26,13 +19,13 @@ window.addEventListener('load', () => {
 })
 
 
-var quizLevel = [{
+const quizLevel = [{
         question: "What does HTML stand for?",
         a: "Hyper Text Markup Language",
         b: "Hyperlink Text Markup Language",
         c: "Hyper Text Manipulation Language",
         d: "HyperText Transmission Port",
-        correct: "a",
+        correctAnswer: "a",
     },
     {
         question: "What is the most widely used programming language for web development?",
@@ -40,7 +33,7 @@ var quizLevel = [{
         b: "Python",
         c: "PHP",
         d: "SQL",
-        correct: "a",
+        correctAnswer: "a",
     },
     {
         question: "What is the correct syntax for an if statement in JavaScript?",
@@ -48,7 +41,7 @@ var quizLevel = [{
         b: "if (i == 5)",
         c: "if i = 5",
         d: "if (i == 5) then",
-        correct: "b",
+        correctAnswer: "b",
     },
     {
         question: "Which of these is a high-level programming language?",
@@ -56,7 +49,7 @@ var quizLevel = [{
         b: "C++",
         c: "Assembly",
         d: "Python",
-        correct: "d",
+        correctAnswer: "d",
     },
     {
         question: "What does CSS stand for?",
@@ -64,17 +57,17 @@ var quizLevel = [{
         b: "Computer Style Sheets",
         c: "Cascading Sheet Styles",
         d: "Computer Sheet Styles",
-        correct: "a",
+        correctAnswer: "a",
     },
 ];
 
-var quizLevelTwo = [{
+const quizLevelTwo = [{
         question: "Who holds the record for the most home runs in a single MLB season?",
         a: "Barry Bonds",
         b: "Mark McGwire",
         c: "Sammy Sosa",
         d: " Babe Ruth",
-        correct: "a",
+        correctAnswer: "a",
     },
     {
         question: "Which country has won the most FIFA World Cup tournaments?",
@@ -82,7 +75,7 @@ var quizLevelTwo = [{
         b: "Germany",
         c: "Italy",
         d: "Argentina",
-        correct: "a",
+        correctAnswer: "a",
     },
     {
         question: "Who is considered the greatest basketball player of all time?",
@@ -90,7 +83,7 @@ var quizLevelTwo = [{
         b: "Lebron James",
         c: "Kareem Abdul-Jabbar",
         d: "Magic Johnson",
-        correct: "a",
+        correctAnswer: "a",
     },
     {
         question: "What is the name of the annual tennis tournament played on grass courts in England?",
@@ -98,7 +91,7 @@ var quizLevelTwo = [{
         b: "Wimbledon",
         c: "US Open",
         d: "Australian Open",
-        correct: "b",
+        correctAnswer: "b",
     },
     {
         question: "Who holds the record for the most career goals in the NHL?",
@@ -106,18 +99,18 @@ var quizLevelTwo = [{
         b: "Mario Lemieux",
         c: "Bobby Orr",
         d: "Steve Yzerman",
-        correct: "a",
+        correctAnswer: "a",
     },
 ];
 
-var quizLevelThree = [{
+const quizLevelThree = [{
         question: "What is the capital of France?",
 
         a: "London",
         b: "Paris",
         c: "Berlin",
         d: "Rome",
-        correct: "b",
+        correctAnswer: "b",
     },
     {
         question: "What is the largest ocean in the world?",
@@ -125,7 +118,7 @@ var quizLevelThree = [{
         b: "Indian Ocean",
         c: "Southern Ocean",
         d: "Pacific Ocean",
-        correct: "d",
+        correctAnswer: "d",
 
     },
     {
@@ -134,7 +127,7 @@ var quizLevelThree = [{
         b: "Buzz Aldrin",
         c: "Michael Collins",
         d: "John Young",
-        correct: "a",
+        correctAnswer: "a",
 
     },
     {
@@ -143,7 +136,7 @@ var quizLevelThree = [{
         b: "Elephant",
         c: "Hippopotamus",
         d: "Rhinoceros",
-        correct: "a",
+        correctAnswer: "a",
     },
     {
         question: "What is the smallest country in the world?",
@@ -151,14 +144,22 @@ var quizLevelThree = [{
         b: "Monaco",
         c: "San Marino",
         d: "Vatican City",
-        correct: "d",
+        correctAnswer: "d",
     },
 ];
 
 
+var timer = document.getElementById("timer");
+const question = document.getElementById("question-text");
+const answerEls = document.querySelectorAll(".answer");
+const optionA = document.getElementById("a-text");
+const optionB = document.getElementById("b-text");
+const optionC = document.getElementById("c-text");
+const optionD = document.getElementById("d-text");
+const submit = document.getElementById("submit");
+
 let currentQuiz = 0;
-let userscore = 0;
-let scoreOne = 0;
+let score = 0;
 let level = 1;
 
 var questionsCount = 0;
@@ -167,72 +168,113 @@ var quizData = [quizLevel, quizLevelTwo, quizLevelThree];
 
 
 quizLoad();
+// startTimer();
 
-function quizLoad(startTimer) {
+function quizLoad() {
     questionsCount += 1;
-    unselectedAnswer();
+    wrongSelection();
 
-    const quizIndex = quizData[quiz][currentQuiz];
 
-    question.innerText = quizIndex.question;
-    optionA.innerText = quizIndex.a;
-    optionB.innerText = quizIndex.b;
-    optionC.innerText = quizIndex.c;
-    optionD.innerText = quizIndex.d;
+    const currentQuizData = quizData[quiz][currentQuiz];
+
+    question.innerText = currentQuizData.question;
+    optionA.innerText = currentQuizData.a;
+    optionB.innerText = currentQuizData.b;
+    optionC.innerText = currentQuizData.c;
+    optionD.innerText = currentQuizData.d;
+
 }
 
 
-function selectedAnswer() {
+function getSelected() {
     let answer = undefined;
 
-    questionAns.forEach((questionAns) => {
-        if (questionAns.checked) {
-            answer = questionAns.id;
-            console.log("answer is: " + answer)
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+            console.log(answer)
         }
     });
     return answer;
 }
 
-function unselectedAnswer() {
-    questionAns.forEach((answer) => {
+
+function wrongSelection() {
+    answerEls.forEach((answer) => {
         answer.checked = false;
+        // console.log("PLEASE WORK " + answer.checked)
     });
 }
 
 
 submit.addEventListener('click', () => {
-    const answer = selectedAnswer();
-    console.log(answer);
+    const answer = getSelected();
 
+    if (answer) {
+        console.log(quizLevel[currentQuiz].correctAnswer);
+        console.log(answer)
+        if (answer === quizLevel[currentQuiz].correctAnswer) {
+            score++;
+            console.log(score);
+        }
+        currentQuiz++;
 
-    // if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
-        score++;
-    }
-
-    currentQuiz++;
-
-    if (currentQuiz < quizLevel.length) {
-        quizLoad();
-
-    } else {
-        if (userscore >= 3) {
-            question.innerText = `Hurray, you have completed Level ${level}. \nYou scored ${userscore}/${questionsCount}`
-            level++;
-            quiz += 1;
-            currentQuiz = 0;
-            if (quiz < quizData.length)
-                quizLoad();
-            else
-                question.innerText = `Hurray, you have completed The King's Quiz. \nYou scored ${userscore}/${questionsCount}`
+        if (currentQuiz < quizLevel.length) {
+            quizLoad();
         } else {
-            question.innerText = "You Lost. \nPlay Again?"
+            question.innerText = `Hurray, you have completed Level ${level}. \nYou scored ${score}/${questionsCount}`
+            level++;
+            // if (score >= 3) {
+            //     question.innerText = `Hurray, you have completed Level ${level}. \nYou scored ${score}/${questionsCount}`
+            //     level++;
+            //     quiz += 1;
+            //     currentQuiz = 0;
+            //     if (quiz < quizData.length)
+            //         quizLoad();
+            //     else
+            //         question.innerText = `Hurray, you have completed The King's Quiz. \nYou scored ${score}/${questionsCount}`
+            // } else {
+            //     question.innerText = "You Lost. \nPlay Again?"
+            // }
         }
     }
-    // }
-
 })
+
+
+// submit.addEventListener('click', () => {
+//     const answer = selected();
+//     console.log(answer);
+
+
+//     if (answer) {
+//         console.log("THE FUCKING ANSWER " + answer);
+//         if (answer == quizData[currentQuiz].correct) {
+//             // console.log("THE FUCKING ANSWER " + answer + ' 2');
+//             score++;
+//         }
+
+//         currentQuiz++;
+
+//         if (currentQuiz < quizLevel.length) {
+//             quizLoad();
+
+//         } else {
+//             if (score >= 3) {
+//                 question.innerText = `Hurray, you have completed Level ${level}. \nYou scored ${score}/${questionsCount}`
+//                 level++;
+//                 quiz += 1;
+//                 currentQuiz = 0;
+//                 if (quiz < quizData.length)
+//                     quizLoad();
+//                 else
+//                     question.innerText = `Hurray, you have completed The King's Quiz. \nYou scored ${score}/${questionsCount}`
+//             } else {
+//                 question.innerText = "You Lost. \nPlay Again?"
+//             }
+//         }
+//     }
+
+// })
 
 
 start.addEventListener('click', () => {
@@ -267,7 +309,7 @@ enter.addEventListener('click', (f) => {
 // })
 
 
-const startTimer = () => {
+function startTimer() {
     var timeLeft = 60;
     var countdown = setInterval(function() {
         timeLeft--;
