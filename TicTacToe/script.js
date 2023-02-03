ef794c064a56625bd23fc4a8c2cb4aa097581de2
TubiOb <tubiobaloluwa@gmail.com>
2023-02-03 15:14:53 +0100

    Scored the Player turn

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -4,21 +4,24 @@ window.addEventListener('load', () => {
 })
 
 const play = document.querySelector("#start-game");
-const playerTurn = document.querySelector("#display-player-turn");
 const Tile = document.querySelectorAll(".tile");
 const player1score = document.querySelector(".playerOneScore");
 const player2score = document.querySelector(".playerTwoScore");
 const reset = document.querySelector("#reset");
+const playerTurn = document.querySelector(".playerTurn").innerText;
 
 var player = "X";
-var tiles = [Tile];
+var tiles = ["", "", "", "", "", "", "", "", ""];
 var firstPlayerscore = 0;
 var secondPlayerscore = 0;
 var PlayerX = "X";
 var PlayerO = "O";
-var isPlayerTurn;
+var isPlayer1Turn;
 
-console.log(tiles)
+
+// var getX = playerTurn.innerHTML;
+// console.log(getX, "span dami")
+// console.log(tiles)
 console.log(playerTurn)
 
 play.addEventListener('click', () => {
@@ -30,12 +33,19 @@ reset.addEventListener('click', () => {
 
 })
 
-if (isPlayerTurn) {
-    playerTurn.inner
-}
+
 
 for (var i = 0; i < 9; i++) {
     var btn = "";
     tiles[i] = btn;
     console.log(tiles[i])
+}
+
+
+function Turn() {
+    if (isPlayer1Turn) {
+        playerTurn.innerText = "1";
+    } else {
+        playerTurn.innerText = 2
+    }
 }
