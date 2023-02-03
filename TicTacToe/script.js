2df7b2a365c377210ee7973e10cb1bae431d7cdc
TubiOb <tubiobaloluwa@gmail.com>
2023-02-03 14:35:39 +0100

    Collecting dom elements and collating playee stats and scores

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,8 +1,41 @@
-// window.addEventListener('load', () => {
-//     $("#home").show();
-//     $("#game").hide();
-// })
+window.addEventListener('load', () => {
+    $("#home").show();
+    $("#game").hide();
+})
+
+const play = document.querySelector("#start-game");
+const playerTurn = document.querySelector("#display-player-turn");
+const Tile = document.querySelectorAll(".tile");
+const player1score = document.querySelector(".playerOneScore");
+const player2score = document.querySelector(".playerTwoScore");
+const reset = document.querySelector("#reset");
+
+var player = "X";
+var tiles = [Tile];
+var firstPlayerscore = 0;
+var secondPlayerscore = 0;
+var PlayerX = "X";
+var PlayerO = "O";
+var isPlayerTurn;
+
+console.log(tiles)
+console.log(playerTurn)
+
+play.addEventListener('click', () => {
+    $("#home").hide();
+    $("#game").show();
+})
+
+reset.addEventListener('click', () => {
+
+})
+
+if (isPlayerTurn) {
+    playerTurn.inner
+}
 
 for (var i = 0; i < 9; i++) {
-
+    var btn = "";
+    tiles[i] = btn;
+    console.log(tiles[i])
 }
