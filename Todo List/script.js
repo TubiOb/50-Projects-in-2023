6592b408791221db38b7677429bb9ec1f62fa6b0
TubiOb <tubiobaloluwa@gmail.com>
2023-02-05 08:05:09 +0100

    Javascript Queries

---
+++ Todo List/script.js
@@ -0,0 +1,8 @@
+// window.addEventListener('load', () => {
+//     $("#Home").show();
+//     $("#TodoList").hide();
+// })
+
+const HomePage = document.querySelector("#container");
+const username = document.querySelector("#myname");
+const usermail = document.querySelector("#mymail");
