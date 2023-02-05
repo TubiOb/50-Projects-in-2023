a07308f7ae95fe7f17e1173165f8dafb43a78096
TubiOb <tubiobaloluwa@gmail.com>
2023-02-05 22:50:50 +0100

    Cards showing. Trying to solve empty card showing.

--- Todo List/script.js
+++ Todo List/script.js
@@ -11,8 +11,9 @@ const Mail = document.querySelector("#Mail");
 const date = document.querySelector("#Date");
 const Title = document.querySelector("#title");
 const tasks = document.querySelector("#contents");
-const addItem = document.querySelector(".material-symbols-outlined");
+const addItem = document.querySelector(".add-item-btn");
 var Taskcard = document.querySelector("#tasksCard");
+const container = document.querySelector("#tasks-container");
 
 const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 var Daydate = new Date().toLocaleDateString();
@@ -37,30 +38,58 @@ login.addEventListener('click', () => {
     }
 })
 
-addItem.addEventListener('click', () => {
-    const task = [];
-    while (true) {
-        if (!tasks.value)
-            break;
-        task.push(tasks);
-    }
 
-    if (task.length > 0) {
-        const card = document.createElement("div");
-        card.classList.add("card");
+
+addItem.addEventListener('click', () => {
+    if (Title.value.length > 0 && tasks.value.length > 0) {
+        const Taskcard = document.createElement('div');
+        // const card = document.createElement('div');
+        Taskcard.classList.add('card');
+        Taskcard.id = "tasksCard";
 
         const getTitle = document.createElement("h4");
         getTitle.innerHTML = Title.value;
-        card.appendChild(getTitle);
+        Taskcard.appendChild(getTitle);
 
-        let tasksHTML = "";
-        task.forEach(tasks => {
-            tasksHTML += `<div class="task">
-        <input type="checkbox">
-        <span>${tasks.value}</span>
-      </div>`;
-        });
-        card.innerHTML += tasksHTML;
-        Taskcard.appendChild(card);
+        const taskHTML = `<div class="task">
+      <input type="checkbox">
+      <span>${tasks.value}</span>
+    </div>`;
+
+        Taskcard.innerHTML += taskHTML;
+
+        // container.appendChild(card);
+
+        container.appendChild(Taskcard);
+
+        Title.value = '';
+        tasks.value = '';
+        $(".card").show();
     }
 });
+
+
+
+// const task = [];
+// while (true) {
+//     if (!tasks.value)
+//         break;
+//     task.push(tasks);
+// }
+
+// if (task.length > 0) {
+//     const card = document.createElement("div");
+//     container.classList.add("ccntainer");
+
+//     
+
+//     let tasksHTML = "";
+//     task.forEach(tasks => {
+//         tasksHTML += `<div class="task">
+//         <input type="checkbox">
+//         <span>${tasks.value}</span>
+//       </div>`;
+//     });
+//     card.innerHTML += tasksHTML;
+//     Taskcard.appendChild(card);
+// }
