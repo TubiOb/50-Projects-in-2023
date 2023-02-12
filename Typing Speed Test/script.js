97319b068462a9042508b8a0599da8c780e8dcdb
TubiOb <tubiobaloluwa@gmail.com>
2023-02-12 03:47:52 +0100

    Typing Test Page

--- Typing Speed Test/script.js
+++ Typing Speed Test/script.js
@@ -2,3 +2,20 @@
 //     var url = '/index.html';
 //     window.open(url, '_self');
 // });
+
+
+
+
+const signUpButton = document.querySelector('#signUpButton');
+const signInButton = document.querySelector('#signInButton');
+const Home = document.querySelector('#Home');
+
+signUpButton.addEventListener('click', () => {
+    console.log("SIGN UP");
+    Home.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    console.log("SIGN OUT");
+    Home.classList.remove("right-panel-active");
+});
