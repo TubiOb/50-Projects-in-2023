062afeef787bcec457ddd824c2c6bfcf462e1a69
TubiOb <tubiobaloluwa@gmail.com>
2023-03-01 17:51:36 +0100

    Navbar mobile completed

---
+++ Animation Website/js/script.js
@@ -0,0 +1,22 @@
+// const navbarToggle = document.querySelector('.navbar-toggle');
+// const navbarMenu = document.querySelector('.navbar');
+
+// navbarToggle.addEventListener('click', () => {
+//     console.log("WOW")
+//     navbarToggle.classList.toggle('active');
+//     navbarMenu.classList.toggle('active');
+// });
+
+
+// navbarToggle.addEventListener('mouseenter', () => {
+//     console.log("SHITE")
+//     navbarToggle.classList.toggle('active');
+//     navbarMenu.classList.toggle('active');
+// });
+
+
+// navbarToggle.addEventListener('mouseleave', () => {
+//     console.log("skrep")
+//     navbarToggle.classList.toggle('active');
+//     navbarMenu.classList.toggle('active');
+// })
