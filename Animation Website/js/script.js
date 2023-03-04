be2ac1fe6d0c51be96b7232314db8d8845759164
TubiOb <tubiobaloluwa@gmail.com>
2023-03-04 23:00:49 +0100

    Finally commiting again

--- Animation Website/js/script.js
+++ Animation Website/js/script.js
@@ -22,3 +22,41 @@ navbarToggle.addEventListener('mouseleave', () => {
     navbarMenu.style.display = "none"
     navbarMenu.classList.toggle('active');
 })
+
+// ACTIVE SCROLL SECTION
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+// SHOW SCROLL UP
+const scrollUp = () => {
+    const scrollUp = document.querySelector('#scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+// CHANGE BACKGROUND HEADER
+const scrollHeader = () => {
+    const header = document.querySelector('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header')
+}
+
+window.addEventListener('scroll', scrollHeader);
