ea96a95ed0fcee743802fecaaaf613f9a4195387
TubiOb <tubiobaloluwa@gmail.com>
2023-03-06 22:03:13 +0100

    Adding swiper class to the cards and dynamically creatin the rest

--- Animation Website/js/script.js
+++ Animation Website/js/script.js
@@ -64,3 +64,13 @@ const scrollHeader = () => {
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+
+
+// CARD SWIPER
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+});
