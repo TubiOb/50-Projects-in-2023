726e1c0f35b1096569c6e51e5470cf2d0cd3a25a
Obaloluwa Tubi <Tubiobaloluwa@gmail.com>
2023-05-11 16:59:42 +0100

    commit

--- Educa - School Website - Watch_and_Code/js/script.js
+++ Educa - School Website - Watch_and_Code/js/script.js
@@ -5,3 +5,68 @@ menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+
+// SWIPER JS
+var swiper = new Swiper(".course-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".teachers-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
